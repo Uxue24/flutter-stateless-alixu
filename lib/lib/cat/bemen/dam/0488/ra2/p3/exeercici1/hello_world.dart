851a74a787bb0dc29a6f85d80c12a1b5ba27b5c2
uxue <uxeslo24@bemen3.cat>
2025-10-27 18:05:19 +0100

    Ex1

--- lib/lib/cat/bemen/dam/0488/ra2/p3/exeercici1/hello_world.dart
+++ lib/lib/cat/bemen/dam/0488/ra2/p3/exeercici1/hello_world.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HelloWorldApp());
+  runApp(const HelloWorldApp()); //inicia l'aplicació amb el widget HelloWorldApp
 }
 
-class HelloWorldApp extends StatelessWidget {
-  const HelloWorldApp({super.key});
+class HelloWorldApp extends StatelessWidget { 
+  const HelloWorldApp({super.key});//constructor
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Exercici 1.1 - Hello World'),
-          backgroundColor: Colors.deepPurple,
+  Widget build(BuildContext context) { //mètode build
+    return MaterialApp( //widget MaterialApp
+      debugShowCheckedModeBanner: false, //elimina la bandera de debug
+      home: Scaffold( 
+        appBar: AppBar( 
+          title: const Text('Exercici 1.1 - Hello World'), //títol de l'aplicació
+          backgroundColor: Colors.deepPurple, //color de la barra d'aplicació
         ),
-        body: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-          ),
+        body: const Center(  
+          child: Text( 
+            'Hello World!', 
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold), //estil del text
+          ), 
         ),
       ),
     );
