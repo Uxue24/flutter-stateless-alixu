d681a1ad167ce53bbefdb09a714a794e8e13eca7
uxue <uxeslo24@bemen3.cat>
2025-10-27 18:05:30 +0100

    EX3

--- lib/lib/cat/bemen/dam/0488/ra2/p3/exeercici1/main_3.dart
+++ lib/lib/cat/bemen/dam/0488/ra2/p3/exeercici1/main_3.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Resource App',
+      title: ' App Ex3',
       home: const ResourcePage(),
     );
   }
@@ -20,28 +20,28 @@ class ResourcePage extends StatelessWidget {
   const ResourcePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return Scaffold(
-      appBar: AppBar(title: const Text('Resource Example')),
+      appBar: AppBar(title: const Text('Resource Example')), // Barra d'aplicació
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0), // Padding al voltant del contingut
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start, // text alineat a l'esquerra
           children: [
             Column(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisSize: MainAxisSize.min, 
               crossAxisAlignment: CrossAxisAlignment.start, // alinea text i imatge a l'esquerra
               children: [
                 const Text(
                   'This is a resource string',
-                  style: TextStyle(fontSize: 18),
+                  style: TextStyle(fontSize: 18), // Estil del text
                 ),
                 const SizedBox(height: 10),
                 // La imatge centrada respecte al text
                 Align(
                   alignment: Alignment.centerLeft, // pots canviar a center per centrar exactament
                   child: Image.network(
-                    'https://i.imgur.com/8Km9tLL.jpg',
+                    'https://i.imgur.com/8Km9tLL.jpg', // URL de la imatge
                     width: 150,
                     height: 150,
                     fit: BoxFit.cover,
